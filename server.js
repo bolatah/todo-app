4e9628b9fb988b668852372f83dd7895796f8476
Bolat <bolatah@outlook.com>
2024-07-18 17:19:09 +0200

    change start script in package.json

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/angular-todo-probearbeit/browser/index.html'));
 });
 
+
+
+// Set the PORT environment variable for the Go server
+process.env.PORT = process.env.PORT || '9002';  // Default to '9002' if not set
+
+// Start the Go server
+const goServer = exec('./goServer', { env: process.env });
+
+goServer.stdout.on('data', (data) => {
+  console.log(`Go server stdout: ${data}`);
+});
+
+goServer.stderr.on('data', (data) => {
+  console.error(`Go server stderr: ${data}`);
+});
+
+goServer.on('close', (code) => {
+  console.log(`Go server process exited with code ${code}`);
+});
+
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
