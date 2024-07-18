0cffc041748d0afc6f5b451b3966901e72ebec51
Bolat <bolatah@outlook.com>
2024-07-18 22:00:18 +0200

    change start script in package.json

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -9,7 +9,7 @@ import { AuthState, selectIsAuthenticated } from '../store/selectors/auth.select
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'https://todo-app-with-angular-18-9db2a73c8e74.herokuapp.com/login';
+  private apiUrl = 'http://localhost:9002/login';
  
   constructor(
     private httpClient: HttpClient,
