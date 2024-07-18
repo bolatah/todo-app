ad26bb53224f0182f135a6e2ad430fddc8e758e5
Bolat <bolatah@outlook.com>
2024-07-18 22:39:10 +0200

    change start script in package.json

--- src/app/store/actions/todos.actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Todo } from '../models/todos.models';
-
-export const loadTodos = createAction('[Todos] Load Todos', props<{ todos: Todo[] }>());
-export const addTodo = createAction('[Todos] Add Todo', props<{ todo: Todo }>());
-export const updateTodo = createAction('[Todos] Update Todo', props<{ todo: Todo }>());
-export const deleteTodo = createAction('[Todos] Delete Todo', props<{ id: number }>());
